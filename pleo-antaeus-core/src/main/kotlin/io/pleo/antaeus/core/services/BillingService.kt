214ea31e5034d3a671a6e9f6e22260d298b5b815
Sebastian Stjerne <s.stjerne@gmail.com>
2021-04-12 17:36:18 -0300

    Introduce BillingService in order to process the pending invoices to paid

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,49 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.InvoiceStatus
+import io.pleo.antaeus.models.Invoice
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val paymentProvider: PaymentProvider,
+    private val invoiceService: InvoiceService
+
 ) {
-// TODO - Add code e.g. here
+    /*
+     * Charges all the pending invoices and return the list of paid invoices
+     */
+    fun chargePendingInvoices(): List<Invoice> {
+        val invoices = invoiceService.fetchInvoicesByStatus(InvoiceStatus.PENDING)
+        val paidInvoices: List<Invoice> = mutableListOf()
+        mutableListOf<Invoice>()
+        for (invoice in invoices) {
+            try {
+                var paid = paymentProvider.charge(invoice)
+                if (paid) {
+                    invoiceService.setInvoice(invoice.id, InvoiceStatus.PAID)
+                    logger.info {"Invoice " + invoice.id + " successfully paid!"}
+                } else {
+                    //TODO: Provide any other status as 'Failed' to avoid to process again, or provide another treat
+                    logger.warn {"Invoice " + invoice.id + " cannot be paid because customer account balance " + invoice.customerId + "  did not allow the charge"}
+                }
+            } catch (e: CustomerNotFoundException) {
+                logger.error(e) {"Invoice " + invoice.id + " cannot be paid because customer " + invoice.customerId + " doesn't found or not exist"}
+            } catch (e: CurrencyMismatchException) {
+                logger.error(e) {"Invoice " + invoice.id + " cannot be paid because currency doesn't match the one of the customer " + invoice.customerId}
+            } catch (e: NetworkException) {
+                logger.error(e) {"Invoice " + invoice.id + " cannot be paid because of a network issue"}
+            } catch (e : InvoiceNotFoundException) {
+                logger.error(e) {"Invoice " + invoice.id + " doesn't found or not exist"}
+            }
+        }
+        return paidInvoices
+    }
+
 }
--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -1,4 +1,80 @@
 package io.pleo.antaeus.core.services
 
+import io.mockk.every
+import io.mockk.mockk
+import io.pleo.antaeus.core.exceptions.NetworkException
+import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+
 class BillingServiceTest {
+
+
+    private val dal = mockk<AntaeusDal> {
+
+        var invoices =  mutableListOf<Invoice>(
+                Invoice(id = 6, customerId = 3, amount = Money(BigDecimal.TEN, Currency.EUR), status = InvoiceStatus.PENDING),
+                Invoice(id = 7, customerId = 3, amount = Money(BigDecimal.TEN, Currency.EUR), status = InvoiceStatus.PAID),
+                Invoice(id = 8, customerId = 4, amount = Money(BigDecimal.TEN, Currency.EUR), status = InvoiceStatus.PENDING),
+                Invoice(id = 9, customerId = 4, amount = Money(BigDecimal.TEN, Currency.EUR), status = InvoiceStatus.PENDING),
+                Invoice(id = 10, customerId = 5, amount = Money(BigDecimal.TEN, Currency.EUR), status = InvoiceStatus.PENDING)
+
+        )
+        every { fetchInvoices() } returns invoices
+
+        every { fetchInvoice(any()) } answers {
+            val id = firstArg<Int>()
+            invoices.filter { invoice ->  invoice.id == id}.first()
+        }
+
+        every { updateInvoiceStatus(any(), any()) } answers {
+            var invo = invoices.filter { invoice ->  invoice.id == firstArg<Int>() }.first()
+            val ninv = invo.copy(status = InvoiceStatus.PAID)
+            invoices.remove(invo)
+            invoices.add(ninv)
+        }
+
+        every { fetchInvoicesByStatus(any()) } answers {
+            val inv = firstArg<InvoiceStatus>()
+            invoices.filter { invoice ->  invoice.status == inv}
+        }
+    }
+
+    private val paymentProvider = mockk<PaymentProvider> ()
+
+    private val billingService = BillingService(invoiceService = InvoiceService(dal = dal), paymentProvider = paymentProvider)
+
+    private val invoiceService = InvoiceService(dal = dal)
+
+
+    @Test
+    fun `network exception`() {
+        every { paymentProvider.charge(any()) } throws NetworkException()
+
+        billingService.chargePendingInvoices()
+
+        val invoicesPaid = invoiceService.fetchInvoicesByStatus(InvoiceStatus.PAID)
+        assertEquals(1, invoicesPaid.size)
+        val invoicesPending = invoiceService.fetchInvoicesByStatus(InvoiceStatus.PENDING)
+        assertEquals(4, invoicesPending.size)
+    }
+
+    @Test
+    fun `successfully charge`() {
+        every { paymentProvider.charge(any()) } returns true
+
+        billingService.chargePendingInvoices()
+
+        val invoicesPaid = invoiceService.fetchInvoicesByStatus(InvoiceStatus.PAID)
+        assertEquals(5, invoicesPaid.size)
+        val invoicesPending = invoiceService.fetchInvoicesByStatus(InvoiceStatus.PENDING)
+        assertEquals(0, invoicesPending.size)
+
+    }
+
+
+
 }
